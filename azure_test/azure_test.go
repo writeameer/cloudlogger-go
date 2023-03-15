018d2d49772521c196bbb8b3f30f615a61fce292
Ameer Deen <writeameer@gmail.com>
2023-03-15 12:05:07 +0000

    Removed dead code

--- azure_test/azure_test.go
+++ azure_test/azure_test.go
@@ -8,17 +8,6 @@ import (
 	"github.com/writeameer/cloudlogger-go/azure"
 )
 
-// func TestHello(t *testing.T) {
-// 	m := azure.Hello("bob")
-// 	fmt.Println(m)
-// }
-
-// func TestHello2(t *testing.T) {
-// 	m := azure.Hello("mo")
-// 	fmt.Println(m)
-// 	os.Exit(1)
-// }
-
 func TestLogtoAzureBlobStorage(t *testing.T) {
 
 	accountName := os.Getenv("AZURE_STORAGE_ACCOUNTNAME")
